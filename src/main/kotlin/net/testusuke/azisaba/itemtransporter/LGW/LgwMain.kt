46ae09705f8810f58300ee17b675f8d8ff47cf44
owner <forgotten_saga_1129@outlook.jp>
2020-08-15 23:09:12 +0900

    [add] create pg/lgw main class

---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/LgwMain.kt
@@ -0,0 +1,8 @@
+package net.testusuke.azisaba.itemtransporter.LGW
+
+/**
+ * Created by testusuke on 2020/08/15
+ * @author testusuke
+ */
+class LgwMain {
+}
---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/PG/PgMain.kt
@@ -0,0 +1,8 @@
+package net.testusuke.azisaba.itemtransporter.PG
+
+/**
+ * Created by testusuke on 2020/08/15
+ * @author testusuke
+ */
+class PgMain {
+}
