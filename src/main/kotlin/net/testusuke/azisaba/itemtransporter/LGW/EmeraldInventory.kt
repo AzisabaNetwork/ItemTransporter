aca780efd80e697671d7ca0efec2f99964894baf
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 19:49:54 +0900

    [Add] add inventory class for put emeralds

---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/EmeraldInventory.kt
@@ -0,0 +1,53 @@
+package net.testusuke.azisaba.itemtransporter.LGW
+
+import net.testusuke.azisaba.itemtransporter.Main
+import net.testusuke.azisaba.itemtransporter.Main.Companion.prefix
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
+
+/**
+ * Created by testusuke on 2020/08/16
+ * @author testusuke
+ */
+object EmeraldInventory {
+
+    //  Inventory Name
+    const val INVENTORY_NAME = "§eItemTransporter §dエメラルドを入れてください"
+
+    fun open(player: Player){
+        val inv = createInventory(player.uniqueId.toString())
+        if(inv == null){
+            player.sendMessage("${prefix}§cエラーが発生しました。")
+            return
+        }
+        //  open Inventory
+        player.openInventory(inv)
+    }
+
+    private fun createInventory(uuid:String):Inventory?{
+        val inventory = Bukkit.createInventory(null,27, INVENTORY_NAME)
+        //  Glass
+        val glassIndex = arrayOf(18,19,20,21,23,25,26)
+        val glass = ItemStack(Material.STAINED_GLASS,1,5)
+        val glassMeta = glass.itemMeta
+        glassMeta.displayName = ""
+        glass.itemMeta = glassMeta
+        for(i in glassIndex) {   //  put
+            inventory.setItem(i,glass)
+        }
+
+        //  amount that has emerald
+        val dataBaseEmeraldAmount = DataBase.getAmount(uuid) ?: return null
+        val hasAmount = ItemStack(Material.EMERALD)
+        val hasMeta = hasAmount.itemMeta
+        hasMeta.displayName = "§aあなたが所持しているエメラルド: §6$dataBaseEmeraldAmount 個"
+        hasAmount.itemMeta = hasMeta
+        inventory.setItem(22,hasAmount)
+
+        return inventory
+    }
+
+}
