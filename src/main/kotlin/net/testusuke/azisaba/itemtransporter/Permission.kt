b7ed14a14e55bbdd026658b8b2f7929c26955970
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 09:23:34 +0900

    [add] add permission

---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/Permission.kt
@@ -0,0 +1,18 @@
+package net.testusuke.azisaba.itemtransporter
+
+/**
+ * Created by testusuke on 2020/08/16
+ * @author testusuke
+ */
+object Permission {
+
+    //  Main
+    const val MAIN_ADMIN = ""
+    //  LGW
+    const val LGW_GENERAL = ""
+    const val LGW_ADMIN = ""
+    //  PG
+    const val PG_GENERAL = ""
+    const val PG_ADMIN = ""
+
+}
