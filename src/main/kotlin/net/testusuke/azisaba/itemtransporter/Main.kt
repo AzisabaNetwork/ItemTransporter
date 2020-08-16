9f89f321321f66be549aa207d4be791deb94b8ee
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 10:48:08 +0900

    [add] add sql

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/Main.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/Main.kt
@@ -52,4 +52,64 @@ class Main:JavaPlugin() {
 
     }
 
+    //  SQL
+    //  emerald
+    val emeraldSQL = "create table item_transport_emerald\n" +
+            "(\n" +
+            "    id int auto_increment,\n" +
+            "    uuid varchar(36) not null,\n" +
+            "    mcid varchar(20) not null,\n" +
+            "    amount int not null,\n" +
+            "    constraint item_transport_emerald_pk\n" +
+            "        primary key (id)\n" +
+            ");"
+    val itemSQL = "create table item_transport_item\n" +
+            "(\n" +
+            "    id int auto_increment,\n" +
+            "    uuid varchar(36) not null,\n" +
+            "    mcid varchar(20) not null,\n" +
+            "    item_id int not null,\n" +
+            "    item_base64 text not null,\n" +
+            "    date datetime not null,\n" +
+            "    constraint item_transport_item_pk\n" +
+            "        primary key (id)\n" +
+            ");"
+    val itemListSQL = "create table item_transport_item_list\n" +
+            "(\n" +
+            "    id int auto_increment,\n" +
+            "    base64 text not null,\n" +
+            "    price int null,\n" +
+            "    available boolean not null,\n" +
+            "    constraint item_transport_item_list_pk\n" +
+            "        primary key (id)\n" +
+            ");"
+    val logSQL = "create table item_transport_log\n" +
+            "(\n" +
+            "    id int auto_increment,\n" +
+            "    uuid varchar(36) not null,\n" +
+            "    mcid varchar(20) null,\n" +
+            "    server varchar(10) not null,\n" +
+            "    action text not null,\n" +
+            "    amount int not null,\n" +
+            "    constraint item_transport_log_pk\n" +
+            "        primary key (id)\n" +
+            ");"
+    private fun createTable(){
+        val connection = database.getConnection()
+        if(connection == null){
+            plugin.logger.info("Can't get connection!")
+            return
+        }
+        //  Statement
+        val statement = connection.createStatement()
+        //  execute
+        statement.execute(emeraldSQL)
+        statement.execute(itemSQL)
+        statement.execute(itemListSQL)
+        statement.execute(logSQL)
+
+        //  close
+        statement.close()
+        connection.close()
+    }
 }
