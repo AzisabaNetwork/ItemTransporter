46a3ec5f2e9d65152f0e7218ef6210d81234789c
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 19:46:01 +0900

    [Update] Converter change Inventory to ItemStack

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/Base64Converter.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/Base64Converter.kt
@@ -1,7 +1,5 @@
 package net.testusuke.azisaba.itemtransporter
 
-import org.bukkit.Bukkit
-import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import org.bukkit.util.io.BukkitObjectInputStream
 import org.bukkit.util.io.BukkitObjectOutputStream
@@ -11,40 +9,27 @@ import java.io.ByteArrayOutputStream
 import java.io.IOException
 
 object Base64Converter {
-    fun toBase64(inventory: Inventory): String? {
+
+    fun toBase64(itemStack: ItemStack): String? {
         return try {
             val outputStream = ByteArrayOutputStream()
             val dataOutput = BukkitObjectOutputStream(outputStream)
-
-            // Write the size of the inventory
-            dataOutput.writeInt(inventory.size)
-
-            // Save every element in the list
-            for (i in 0 until inventory.size) {
-                dataOutput.writeObject(inventory.getItem(i))
-            }
-
-            // Serialize that array
+            dataOutput.writeInt(1)
+            dataOutput.writeObject(itemStack)
             dataOutput.close()
             Base64Coder.encodeLines(outputStream.toByteArray())
         } catch (e: Exception) {
             throw IllegalStateException("Unable to save item stacks.", e)
         }
     }
-
     @Throws(IOException::class)
-    fun fromBase64(data: String?): Inventory? {
+    fun fromBase64(data: String?): ItemStack? {
         return try {
             val inputStream = ByteArrayInputStream(Base64Coder.decodeLines(data))
             val dataInput = BukkitObjectInputStream(inputStream)
-            val inventory = Bukkit.getServer().createInventory(null, dataInput.readInt())
-
-            // Read the serialized inventory
-            for (i in 0 until inventory.size) {
-                inventory.setItem(i, dataInput.readObject() as ItemStack)
-            }
+            val itemStack = dataInput.readObject() as ItemStack
             dataInput.close()
-            inventory
+            itemStack
         } catch (e: ClassNotFoundException) {
             throw IOException("Unable to decode class type.", e)
         }
