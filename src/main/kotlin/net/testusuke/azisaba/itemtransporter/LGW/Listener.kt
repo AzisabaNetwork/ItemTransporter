9d81f2a9dd8d13b7ad988f723af6b8fa8bec3b28
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 19:52:54 +0900

    [Update] cancel to click glass items

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Listener.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Listener.kt
@@ -1,8 +1,30 @@
 package net.testusuke.azisaba.itemtransporter.LGW
 
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+
 /**
  * Created by testusuke on 2020/08/16
  * @author testusuke
  */
-object Listener {
+object Listener:Listener {
+
+    //  Inventory
+    @EventHandler
+    fun onClickInventory(event:InventoryClickEvent){
+        if(event.view.title != EmeraldInventory.INVENTORY_NAME)return
+        val player = event.whoClicked as Player
+        val inventory = event.inventory
+        val index = event.slot
+
+        //  Glass Index
+        if(index in 18..26){
+            event.isCancelled = true
+            return
+        }
+
+
+    }
 }
