ab8930304bebf740bb252e22606186dd633c98d2
owner <forgotten_saga_1129@outlook.jp>
2020-08-16 11:13:03 +0900

    [Add] add Vault Manager.

---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/VaultManager.kt
@@ -0,0 +1,46 @@
+package net.testusuke.azisaba.itemtransporter
+
+import net.milkbowl.vault.economy.Economy
+import net.testusuke.azisaba.itemtransporter.Main.Companion.plugin
+
+/**
+ * @author testusuke
+ * Vault連携のためのオブジェクト
+ *
+ * 残高確認
+ * VaultManager.economy?.getBalance(player)
+ * 引き出し
+ * VaultManager.economy?.withdrawPlayer(player,double)
+ * 入金
+ * VaultManager.economy?.depositPlayer(player,double)
+ *
+ */
+object VaultManager {
+    var enableEconomy = true
+        private set
+
+    var economy: Economy? = null
+        private set
+
+    fun setup() {
+        enableEconomy = setupEconomy()
+        if (enableEconomy) plugin.logger.info("Enable Vault")
+    }
+
+    private inline val enableVault
+        get() = plugin.getServer().pluginManager.isPluginEnabled("Vault")
+
+    private fun setupEconomy(): Boolean {
+        if (!enableVault) {
+            plugin.logger.info("Vault is not installed")
+            return false
+        }
+        val rsp = plugin.getServer().servicesManager.getRegistration(Economy::class.java)
+        if (rsp == null) {
+            plugin.logger.info("Can not use Vault service")
+            return false
+        }
+        economy = rsp.provider
+        return true
+    }
+}
