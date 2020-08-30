e656741b65f38c03a677078f9080433ae23fa08e
owner <forgotten_saga_1129@outlook.jp>
2020-08-30 22:28:29 +0900

    [Add] add onCommand function

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Command.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Command.kt
@@ -1,8 +1,16 @@
 package net.testusuke.azisaba.itemtransporter.LGW
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+
 /**
  * Created by testusuke on 2020/08/16
  * @author testusuke
  */
-object Command {
+object Command:CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+
+        return false
+    }
 }
