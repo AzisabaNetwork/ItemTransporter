2a1c90f58e623a202472aad313f2964345cd41cc
owner <forgotten_saga_1129@outlook.jp>
2020-08-31 11:00:06 +0900

    [Add] add SQL Result

---
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/ResultType.kt
@@ -0,0 +1,16 @@
+package net.testusuke.azisaba.itemtransporter
+
+/**
+ * Created by testusuke on 2020/08/30
+ * @author testusuke
+ */
+sealed class ResultType {
+    data class Success(val id: Int) : ResultType()
+    data class Error(val reason: ErrorReason) : ResultType()
+}
+
+enum class ErrorReason {
+    CAN_NOT_ACCESS_DB,
+    FAILED_SQL,
+    ITEM_NOT_FOUND
+}
