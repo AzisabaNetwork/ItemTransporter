1c92093e551a30e5c1f1f5e67fb795f486ee5cbc
owner <forgotten_saga_1129@outlook.jp>
2020-09-10 13:47:48 +0900

    [Add] add general command

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Command.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/LGW/Command.kt
@@ -1,8 +1,13 @@
 package net.testusuke.azisaba.itemtransporter.LGW
 
+import net.testusuke.azisaba.itemtransporter.Main.Companion.prefix
+import net.testusuke.azisaba.itemtransporter.Permission
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 /**
  * Created by testusuke on 2020/08/16
@@ -10,7 +15,33 @@ import org.bukkit.command.CommandSender
  */
 object Command:CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if(sender !is Player)return false
+        //  Permission
+        if(!sender.hasPermission(Permission.LGW_GENERAL)){
+            sender.sendMessage("${prefix}§c権限がありません")
+            return false
+        }
 
+        if(args.isEmpty()){
+            MenuInventory.open(sender)
+            return true
+        }
+
+        when(args[0]){
+            "help" -> sendHelp(sender)
+            "emerald" -> {
+
+            }
+        }
         return false
     }
+
+    private fun sendHelp(player: Player){
+        val msg = """
+            /it
+        """.trimIndent()
+        //  send
+        player.sendMessage(msg)
+    }
+
 }
