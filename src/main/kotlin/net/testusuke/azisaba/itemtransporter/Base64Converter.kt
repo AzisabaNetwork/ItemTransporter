0b8694a2c668b5bcb5a8589583afbb73fa8901f1
owner <forgotten_saga_1129@outlook.jp>
2020-12-30 22:34:55 +0900

    [Update] update base64 converter

--- src/main/kotlin/net/testusuke/azisaba/itemtransporter/Base64Converter.kt
+++ src/main/kotlin/net/testusuke/azisaba/itemtransporter/Base64Converter.kt
@@ -6,45 +6,35 @@ import org.bukkit.util.io.BukkitObjectOutputStream
 import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 object Base64Converter {
 
     ///////////////////////////////
     //base 64
     //////////////////////////////
-    fun itemFromBase64(data: String): ItemStack? = try {
-        val inputStream = ByteArrayInputStream(Base64Coder.decodeLines(data))
-        val dataInput = BukkitObjectInputStream(inputStream)
-        val items = arrayOfNulls<ItemStack>(dataInput.readInt())
-
-        // Read the serialized inventory
-        for (i in items.indices) {
-            items[i] = dataInput.readObject() as ItemStack
-        }
-
-        dataInput.close()
-        items[0]
-    } catch (e: Exception) {
-        null
-    }
-
-    @Throws(IllegalStateException::class)
-    fun itemToBase64(item: ItemStack): String {
-        try {
+    fun toBase64(itemStack: ItemStack): String? {
+        return try {
             val outputStream = ByteArrayOutputStream()
             val dataOutput = BukkitObjectOutputStream(outputStream)
-            val items = arrayOfNulls<ItemStack>(1)
-            items[0] = item
-            dataOutput.writeInt(items.size)
-
-            for (i in items.indices) {
-                dataOutput.writeObject(items[i])
-            }
-
+            dataOutput.writeInt(1)
+            dataOutput.writeObject(itemStack)
             dataOutput.close()
-            return Base64Coder.encodeLines(outputStream.toByteArray())
+            Base64Coder.encodeLines(outputStream.toByteArray())
         } catch (e: Exception) {
             throw IllegalStateException("Unable to save item stacks.", e)
+        }
+    }
+    @Throws(IOException::class)
+    fun fromBase64(data: String?): ItemStack? {
+        return try {
+            val inputStream = ByteArrayInputStream(Base64Coder.decodeLines(data))
+            val dataInput = BukkitObjectInputStream(inputStream)
+            val itemStack = dataInput.readObject() as ItemStack
+            dataInput.close()
+            itemStack
+        } catch (e: ClassNotFoundException) {
+            throw IOException("Unable to decode class type.", e)
         }
     }
 }
